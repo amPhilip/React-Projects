0d6b81966a2f308762a46130ac8a730f9a39eec3
amPhilip <philipmwangi91@gmail.com>
2022-07-02 02:41:33 +0300

    BMI-calc:Initial commit

--- BMI-calc-Hooks/src/App.jsx
+++ BMI-calc-Hooks/src/App.jsx
@@ -1,45 +1,132 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [bmiRes, setBmiRes] = useState("");
+  const [message, setMessage] = useState("");
+  //const [gender, setGender] = useState();
 
+  const inputWeight = (e) => {
+    setWeight(e.target.value);
+  };
+  const inputHeight = (e) => {
+    setHeight(e.target.value);
+  };
+  const handlegender = (e) => {
+    setGender(e.target.value);
+  };
+
+  const handleBMIRes = () => {
+    let bmi = weight / (height * height);
+    if (weight && height === 0) {
+      alert("please Set Values");
+    } else {
+      bmi;
+    }
+    setBmiRes(bmi.toFixed(2));
+
+    if (bmi < 18.5) {
+      setMessage("You are UnderWeight");
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+      setMessage("You have a Normal Weight");
+    } else {
+      setMessage("You are Obese");
+    }
+  };
+
+  const clearResult = () => {
+    setWeight(0);
+    setHeight(0);
+    setBmiRes("");
+    setMessage("");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="calc__app">
+      <section className="App">
+        <header className="header">
+          <div className="logo">
+            <span>
+              <h4>BMI calc</h4>
+            </span>
+          </div>
+        </header>
+
+        <div className="calc__container">
+          <div className="weight">
+            <button>weight</button>
+            <div className="inputWeight">
+              <input
+                type="number"
+                name="weight"
+                id="weightNum"
+                value={weight}
+                onChange={inputWeight}
+              />
+              <h4>Kgs</h4>
+            </div>
+          </div>
+
+          <div className="height">
+            <button>height</button>
+            <div className="inputHeight">
+              <input
+                type="number"
+                name="height"
+                id="heightNum"
+                value={height}
+                onChange={inputHeight}
+              />
+              <h4>mts</h4>
+            </div>
+          </div>
+
+          {/*<div className="gender">
+            <input
+              type="radio"
+              id="male"
+              name="gender"
+              value="male"
+              onChange={handlegender}
+            />
+            <label for="male">Male</label>
+            <br />
+            <input
+              type="radio"
+              id="female"
+              name="gender"
+              value="female"
+              onChange={handlegender}
+            />
+            <label for="female">Female</label>
+            <br />
+          </div>*/}
+
+          <div className="btns">
+            <button className="submit__ans" onClick={handleBMIRes}>
+              Submit
+            </button>
+            <button className="reload" onClick={clearResult}>
+              Reload
+            </button>
+          </div>
+        </div>
+        <hr />
+
+        <div className="result__sect">
+          <div className="bmi__result">
+            <h2>{bmiRes}BMI</h2>
+          </div>
+          <div className="message">
+            <span>
+              <p>{message}</p>
+            </span>
+          </div>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
