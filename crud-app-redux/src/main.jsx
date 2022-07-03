6960207cc3f7fad881732c15fb58ce54f6c93cb5
amPhilip <philipmwangi91@gmail.com>
2022-07-04 02:22:23 +0300

    crud-app: Added fake data adn redux functionality reducers

--- crud-app-redux/src/main.jsx
+++ crud-app-redux/src/main.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import App from "./App";
+import userReducer from "./Redux/Reducer/usersReducer";
+const store = configureStore({
+  reducer:{
+    users:userReducer,
+  }
+})
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
-)
+);
