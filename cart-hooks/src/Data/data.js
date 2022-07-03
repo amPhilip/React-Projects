45e477b6a042fcd7acc1d99f9d29911a551c7314
amPhilip <philipmwangi91@gmail.com>
2022-07-03 05:45:51 +0300

    Cart App: App Data

---
+++ cart-hooks/src/Data/data.js
@@ -0,0 +1,23 @@
+const data = {
+  products: [
+    {
+      id: 1,
+      title: "Ceramic Mugs",
+      img: "https://images.unsplash.com/photo-1611274757139-03ff1736701d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y3Vwc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+      price: 35
+    },
+    {
+      id: 2,
+      title: "Tea Cups",
+      img: "https://images.unsplash.com/photo-1529060207344-c1a879ed7a9c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGN1cHN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+      price: 45
+    },
+    {
+      id: 3,
+      title: "Earth Ceramic Mugs",
+      img: "https://images.unsplash.com/photo-1514228742587-6b1558fcca3d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y3VwfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+      price: 25
+    },
+  ],
+};
+export default data;
