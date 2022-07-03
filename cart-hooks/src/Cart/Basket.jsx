d27b9cd2c32fc7d731b3233d69092207d2cdc29d
amPhilip <philipmwangi91@gmail.com>
2022-07-04 00:00:30 +0300

    Cart app: added total amount section for the product

--- cart-hooks/src/Cart/Basket.jsx
+++ cart-hooks/src/Cart/Basket.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
 const Basket = ({ addtoCart, setAddToCart, addProduct, removeProduct }) => {
+  const productPrice = addtoCart.reduce(
+    (acc, curr) => acc + curr.price * curr.qty,
+    0
+  );
+  const prodTax = productPrice * 0.14;
+  const prodShipping = productPrice > 1200 ? 0 : 100;
+  const totalPrice = productPrice + prodTax + prodShipping;
   return (
     <div className="basket">
       <span className="title">
@@ -13,14 +20,50 @@ const Basket = ({ addtoCart, setAddToCart, addProduct, removeProduct }) => {
         <div key={item.id} className="row">
           <div>{item.title}</div>
           <div>
-            <button onClick={() => removeProduct(item)}>-</button>
             <button onClick={() => addProduct(item)}>+</button>
+            <button onClick={() => removeProduct(item)}>-</button>
           </div>
           <div className="qty">
             {item.qty} x ${item.price.toFixed(2)}
           </div>
         </div>
       ))}
+      <hr />
+      {addtoCart.length !== 0 && (
+        <>
+          <div className="prodPrice">
+            <span>
+              <h4>Your Prod Price</h4>
+            </span>
+            <span>{productPrice.toFixed(1)}</span>
+          </div>
+          <div className="prodTax">
+            <span>
+              <h4>Tax</h4>
+            </span>
+            <span>{prodTax.toFixed(1)}</span>
+          </div>
+          <div className="prodShipping">
+            <span>
+              <h4>Shipping</h4>
+            </span>
+            <span>{prodShipping.toFixed(1)}</span>
+          </div>
+          <div className="prodTotal">
+            <span>
+              <h4>Total Amount</h4>
+            </span>
+            <span>{totalPrice.toFixed(1)}</span>
+          </div>
+          <hr />
+          <button
+            className="checkout"
+            onClick={() => alert("Your payment is Processing")}
+          >
+            CheckOut
+          </button>
+        </>
+      )}
     </div>
   );
 };
