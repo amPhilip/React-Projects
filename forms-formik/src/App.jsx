1021a8a02f6a7d4788dde561678e25d5d5eb2d50
amPhilip <philipmwangi91@gmail.com>
2022-07-04 15:01:50 +0300

    React Projects:Forms using Formik Initial Commit-Starter Files

---
+++ forms-formik/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <span><h3>Formik App</h3></span>
+    </div>
+  )
+}
+
+export default App
